48b35681f99652f11112b0274547a4fae29fb50a
Dean Bartel <300089145@fvtc.edu>
2023-09-06 08:58:47 -0500

    pushing again

--- DDB.Notepad.UI/frmNotepad.Designer.cs
+++ DDB.Notepad.UI/frmNotepad.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             menuStrip1 = new MenuStrip();
             mnuFile = new ToolStripMenuItem();
             mnuFileNew = new ToolStripMenuItem();
@@ -36,6 +37,24 @@
             mnuFileExit = new ToolStripMenuItem();
             editToolStripMenuItem = new ToolStripMenuItem();
             undoToolStripMenuItem = new ToolStripMenuItem();
+            btnTimerToggle = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            lblInfo = new Label();
+            txtInfo = new TextBox();
+            lbxInfo = new ListBox();
+            timer1 = new System.Windows.Forms.Timer(components);
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -44,7 +63,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { mnuFile, editToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(1010, 24);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -58,25 +77,25 @@
             // mnuFileNew
             // 
             mnuFileNew.Name = "mnuFileNew";
-            mnuFileNew.Size = new Size(180, 22);
+            mnuFileNew.Size = new Size(135, 22);
             mnuFileNew.Text = "&New";
             // 
             // mnuFileOpen
             // 
             mnuFileOpen.Name = "mnuFileOpen";
-            mnuFileOpen.Size = new Size(180, 22);
+            mnuFileOpen.Size = new Size(135, 22);
             mnuFileOpen.Text = "&Open";
             // 
             // toolStripMenuItem1
             // 
             toolStripMenuItem1.Name = "toolStripMenuItem1";
-            toolStripMenuItem1.Size = new Size(177, 6);
+            toolStripMenuItem1.Size = new Size(132, 6);
             // 
             // mnuFileExit
             // 
             mnuFileExit.Name = "mnuFileExit";
             mnuFileExit.ShortcutKeys = Keys.Alt | Keys.F4;
-            mnuFileExit.Size = new Size(180, 22);
+            mnuFileExit.Size = new Size(135, 22);
             mnuFileExit.Text = "E&xit";
             mnuFileExit.Click += mnuFileExit_Click;
             // 
@@ -93,11 +112,185 @@
             undoToolStripMenuItem.Size = new Size(103, 22);
             undoToolStripMenuItem.Text = "&Undo";
             // 
+            // btnTimerToggle
+            // 
+            btnTimerToggle.Location = new Point(12, 43);
+            btnTimerToggle.Name = "btnTimerToggle";
+            btnTimerToggle.Size = new Size(99, 23);
+            btnTimerToggle.TabIndex = 1;
+            btnTimerToggle.Text = "ON";
+            btnTimerToggle.UseVisualStyleBackColor = true;
+            btnTimerToggle.Click += btnTimerToggle_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 72);
+            button2.Name = "button2";
+            button2.Size = new Size(99, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 101);
+            button3.Name = "button3";
+            button3.Size = new Size(99, 23);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 130);
+            button4.Name = "button4";
+            button4.Size = new Size(99, 23);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(12, 159);
+            button5.Name = "button5";
+            button5.Size = new Size(99, 23);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 304);
+            button6.Name = "button6";
+            button6.Size = new Size(99, 23);
+            button6.TabIndex = 10;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(12, 275);
+            button7.Name = "button7";
+            button7.Size = new Size(99, 23);
+            button7.TabIndex = 9;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(12, 246);
+            button8.Name = "button8";
+            button8.Size = new Size(99, 23);
+            button8.TabIndex = 8;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(12, 217);
+            button9.Name = "button9";
+            button9.Size = new Size(99, 23);
+            button9.TabIndex = 7;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(12, 188);
+            button10.Name = "button10";
+            button10.Size = new Size(99, 23);
+            button10.TabIndex = 6;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(12, 420);
+            button12.Name = "button12";
+            button12.Size = new Size(99, 23);
+            button12.TabIndex = 14;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(12, 391);
+            button13.Name = "button13";
+            button13.Size = new Size(99, 23);
+            button13.TabIndex = 13;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(12, 362);
+            button14.Name = "button14";
+            button14.Size = new Size(99, 23);
+            button14.TabIndex = 12;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(12, 333);
+            button15.Name = "button15";
+            button15.Size = new Size(99, 23);
+            button15.TabIndex = 11;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            lblInfo.BorderStyle = BorderStyle.FixedSingle;
+            lblInfo.Location = new Point(133, 43);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(433, 197);
+            lblInfo.TabIndex = 15;
+            // 
+            // txtInfo
+            // 
+            txtInfo.Location = new Point(133, 247);
+            txtInfo.Multiline = true;
+            txtInfo.Name = "txtInfo";
+            txtInfo.Size = new Size(433, 196);
+            txtInfo.TabIndex = 16;
+            // 
+            // lbxInfo
+            // 
+            lbxInfo.FormattingEnabled = true;
+            lbxInfo.ItemHeight = 15;
+            lbxInfo.Location = new Point(613, 43);
+            lbxInfo.Name = "lbxInfo";
+            lbxInfo.Size = new Size(368, 409);
+            lbxInfo.TabIndex = 17;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
             // frmNotepad
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1010, 572);
+            Controls.Add(lbxInfo);
+            Controls.Add(txtInfo);
+            Controls.Add(lblInfo);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(btnTimerToggle);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "frmNotepad";
@@ -119,5 +312,23 @@
         private ToolStripMenuItem editToolStripMenuItem;
         private ToolStripMenuItem undoToolStripMenuItem;
         private ToolStripMenuItem mnuFileExit;
+        private Button btnTimerToggle;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Label lblInfo;
+        private TextBox txtInfo;
+        private ListBox lbxInfo;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- DDB.Notepad.UI/frmNotepad.cs
+++ DDB.Notepad.UI/frmNotepad.cs
@@ -11,5 +11,31 @@ namespace DDB.Notepad.UI
         {
             Application.Exit();
         }
+
+        private void btnTimerToggle_Click(object sender, EventArgs e)
+        {
+            // Toggle the button - LONG WAY
+
+            if (btnTimerToggle.Text == "ON")
+            {
+                //Enable Timer and change text to OFF
+                btnTimerToggle.Text = "OFF";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                //Disable Timer and change text to ON
+                btnTimerToggle.Text = "ON";
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // This code will execute every second
+            // when timer is enabled.
+
+            lbxInfo.Items.Add(DateTime.Now.ToString());
+        }
     }
 }
